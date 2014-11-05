71e23e7bd7432f27560143c2c77584ca85d96dc9
Kyle Repinski <repinski23@gmail.com>
2014-11-05 06:56:28 -0600

    Lollipops are delicious.
    Conflicts:
    	init.tuna.rc
    
    Change-Id: Ia9c6ce83849c7e3e032f70c57fcc74ebb29f9d33

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,7 +155,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_RECOVERY_UI_LIB := librecovery_ui_tuna
 
 # device-specific extensions to the updater binary
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_tuna
+# inexplicable build errors with Lollipop...
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_tuna
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_FOLDER)
 
 # use the new recovery.fstab format
